17b920c4ac2b7134de03c190f01efa83878d9902
Fleli <frederikedvardsen@gmail.com>
2023-08-31 12:03:26 +0200

    Wrote clock function for VM

--- VM/VM.java
+++ VM/VM.java
@@ -1,6 +1,9 @@
 
 public class VM {
     
+    private final static int addressBound = 65536;
+    private final static int instructionCount = 32;
+    
     private VMMemory memory;
     
     private int pc = 0;
@@ -10,8 +13,25 @@ public class VM {
     private int rb = 0;
     private int rc = 0;
     
+    private VMInstruction[] instructions;
+    
     public VM() {
+        
         memory = new VMMemory();
+        instructions = new VMInstruction[instructionCount];
+        
+        // Fill instructions ...
+        
+    }
+    
+    public void clock() {
+        
+        pc = (pc + 1) % addressBound;
+        ir = memory.read(pc) % instructionCount;
+        
+        VMInstruction instruction = instructions[ir];
+        instruction.execute();
+        
     }
     
 }
