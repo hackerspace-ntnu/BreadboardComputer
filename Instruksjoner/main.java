e4dfe9a2bf6300dea6b7fc28e5676cf183715281
Fleli <frederikedvardsen@gmail.com>
2024-03-25 19:17:34 +0100

    Deleted old files

--- Instruksjoner/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        mikro_instruksjoner mikro_instruksjoner = new mikro_instruksjoner();
-        mikro_instruksjoner.test_binary_in_java();
-
-        instruksjoner instruksjoner = new instruksjoner();
-        instruksjoner.instruksjonerr();
-    }
-}
--- VM/VMInstruction.java
+++
@@ -1,9 +0,0 @@
-/**
- * A {@code VMInstruction} represents an instruction that the physical CPU can run.
- */
-@FunctionalInterface
-public interface VMInstruction {
-    
-    public void execute();
-    
-}
