65554fb39841cb72270c5428591b9d889592c0fa
viktor <viktorwv@icloud.com>
2025-02-01 16:08:48 +0100

    :wastebasket: move old arduino code

--- ArduinoKode/programprogram.ino
+++ ArduinoKode/programprogram.ino
@@ -5,23 +5,22 @@ const int RCLK = 4;
 const int SCLK = 6;
 const int SER = 7;
 const int SHIFTOUT = 8;
-
 const int RedLED = A7;
 const int BlueLED = A3;
 const int GreenLED = A5;
-
-// Trengs ikke foreløpig
 const int GirSpak = 45;
 // const int TstateReset = 46;
 // const int PCReset = 48;
 const int OUTDISABLE = 51;
 const int MARIN = 52;
 const int RAMIN = 53;
-
 #define SetRed() setColor(255, 0, 0)
 #define SetGreen() setColor(0, 255, 0)
-const char *data[3500] = {
-    "0000000000001000", "0000000000000000", "1000001001001000", "1000000000000000", "0000000001000100", "0100000000000000", "E"};
+const char *data[200] = {
+    "0000000000001000", "0000000000000000",
+    "0000000001001000", "1000000000000000",
+    "0000000001000100", "0100000000000000",
+    "E"};
 char *intToString(int integer, int length)
 {
     char *str = new char[length + 1];
@@ -33,68 +32,59 @@ char *intToString(int integer, int length)
     str[length] = '\0';
     return str;
 }
-
 void MARINCycle()
 {
     digitalWrite(MARIN, HIGH);
-    delay(100);
+    delay(4);
     digitalWrite(MARIN, LOW);
 }
 void RAMINCycle()
 {
     digitalWrite(RAMIN, HIGH);
-    delay(100);
+    delay(4);
     digitalWrite(RAMIN, LOW);
 }
-
 void shiftout_en()
 {
     digitalWrite(OUTDISABLE, LOW);
-    delay(100);
+    delay(4);
     digitalWrite(SHIFTOUT, LOW);
-    delay(100);
+    delay(4);
     digitalWrite(GirSpak, HIGH);
 }
-
 void shiftout_dis()
 {
     digitalWrite(GirSpak, LOW);
     digitalWrite(SHIFTOUT, HIGH);
-    delay(100);
+    delay(4);
     digitalWrite(OUTDISABLE, HIGH);
-    delay(100);
+    delay(4);
 }
-
 // flytter til å matche ir, skiller ikke mellom immidiates og instruksjon så rip
 void shiftBitsToEnd(char *str)
 {
     // Check if the string is null or too short
     if (str == nullptr || strlen(str) < 16)
         return;
-
     // Temporary arrays to hold segments of the string
     char inst[6] = {0};
     char srba[4] = {0};
     char srcb[4] = {0};
     char destin[4] = {0};
-
     // Copy specific ranges of bits/characters from the original string
     strncpy(inst, str + 2, 5);    // Copy bits 2-6 (5 bits)
     strncpy(srba, str + 7, 3);    // Copy bits 7-9 (3 bits)
     strncpy(srcb, str + 10, 3);   // Copy bits 10-12 (3 bits)
     strncpy(destin, str + 13, 3); // Copy bits 13-15 (3 bits)
-
     // Construct the new string with the rearranged parts
     snprintf(str, 17, "%s%s%s%s00", destin, srcb, srba, inst);
 }
-
 void setColor(int redValue, int greenValue, int blueValue)
 {
     analogWrite(RedLED, redValue);
     analogWrite(GreenLED, greenValue);
     analogWrite(BlueLED, blueValue);
 }
-
 // Writes the address into marin
 void SR_writeAddress(char *str)
 {
@@ -110,13 +100,13 @@ void SR_writeAddress(char *str)
         shiftVal = (strToWord & 0x01);
         digitalWrite(SER, shiftVal);
         digitalWrite(SCLK, HIGH);
-        delay(10);
+        delay(4);
         digitalWrite(SCLK, LOW);
-        delay(10);
+        delay(4);
         strToWord = strToWord >> 1;
     }
     digitalWrite(RCLK, HIGH);
-    delay(100);
+    delay(4);
     digitalWrite(RCLK, LOW);
     Serial.println("Shift register latched");
 }
@@ -127,7 +117,6 @@ void SR_writeWord(char *str)
     {
         strToWord = (strToWord << 1) | (str[i] - '0'); // Build the 16-bit word, MSB-first
     }
-
     uint8_t counter;
     for (counter = 0; counter < 16; counter++)
     {
@@ -135,15 +124,13 @@ void SR_writeWord(char *str)
         uint8_t shiftVal = (strToWord >> (15 - counter)) & 0x01;
         digitalWrite(SER, shiftVal);
         digitalWrite(SCLK, HIGH);
-        delay(10); // Adjust this delay if needed
+        delay(4); // Adjust this delay if needed
         digitalWrite(SCLK, LOW);
-        delay(10);
+        delay(4);
     }
-
     digitalWrite(RCLK, HIGH); // Latch the shift register
-    delay(10);
+    delay(4);
     digitalWrite(RCLK, LOW);
-
     Serial.println("Shift register latched");
 }
 void Handler(char **data)
@@ -158,7 +145,7 @@ void Handler(char **data)
             MARINCycle();
             break;
         }
-        Serial.print(addresseCounter);
+        Serial.println(addresseCounter);
         char *addresseCounterStr = intToString(addresseCounter, 16);
         SR_writeAddress(addresseCounterStr);
         Serial.print("Adresse dataen blir puttet i: ");
@@ -171,10 +158,9 @@ void Handler(char **data)
         SR_writeWord(data[i]);
         RAMINCycle();
     }
-    delay(10);
+    delay(4);
     shiftout_dis();
 }
-
 void setup()
 {
     Serial.begin(9600);
@@ -202,7 +188,6 @@ void setup()
     Handler(data);
     SetGreen();
 }
-
 void loop()
 {
 }
